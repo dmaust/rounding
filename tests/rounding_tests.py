2777f9afbeb74eed53caad11019aa2ae1bfb05fd
dmaust <david@dmaust.net>
2013-10-09 23:43:30 -0700

    Adding Unit tests.

---
+++ tests/rounding_tests.py
@@ -0,0 +1,24 @@
+__author__ = 'dmaust'
+
+from nose.tools import *
+import rounding
+import random
+
+def setup():
+    pass
+
+def teardown():
+    pass
+
+def test_standard():
+    standard = rounding.StandardRound()
+    assert standard.round(123.45)==123, "Rounds down normally"
+
+def test_stochastic():
+    stochastic = rounding.StochasticRound(random_generator=random.Random(123))
+    total = sum(stochastic.round(123.45) for i in range(100))
+    assert (total>=12340 and total<=12360), "Rounds down some of the time:%d" % (total,)
+    # error + total = expected
+    eq_(total-stochastic.cumulative_error, 12345)
+
+
